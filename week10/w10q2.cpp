f5400203a59624559387469fe1bc997efa701386
himanshumahaur <himanshumahaur@outlook.com>
2023-10-03 11:33:59 +0530

    optimized w10q2

--- week10/w10q2.cpp
+++ week10/w10q2.cpp
@@ -19,28 +19,8 @@ int getcoin(vector<int>& coins, int amount) {
     return num;
 }
 
-int least(int t) {
-    int res = INT_MAX-1;
-
-    if(t==0) return 0;
-
-    for(int i=0; i<n; i++) {
-        if(t-item[i]<0) break;
-
-        int prev;
-        if(myhash.count(t-item[i])) prev = myhash[t-item[i]];
-        else prev = least(t-item[i]);
-
-        res = min(res, prev);
-    }
-
-    myhash[t] = res+1;
-    return res+1;
-}
-
 int main() {
     cin >> n >> t;
-
     item.resize(n);
 
     for(int i=0; i<n; i++) {
